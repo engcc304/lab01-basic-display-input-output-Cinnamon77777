5e536d500a0e33f26153342582905df83d1d81e7
your name <sriwljitthitima@gmail.com>
2023-07-06 00:20:54 +0700

    up lab01

--- Lab/Lab01-Basic-Display-Input-Output.cpp
+++ Lab/Lab01-Basic-Display-Input-Output.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
-int main() {
+
+int main(){
     char Name[50];
-    int Age = 0;
+    int  Age = 0;
     printf("Enter your name: ");
-    scanf("%s", Name);
+    scanf("%s", &Name);
     printf("Enter your age: ");
     scanf("%d", &Age);
     printf("- - - - - -\n");
     printf("Hello %s\n", Name); //TODO: #14 Say hello to user.
-    printf("Age = %d\n", Age + 1); //TODO: #15 display user their age number plus 1.
-    return 0;
+    printf("Age = %d\n", Age); //TODO: #15 display user their age number plus 1.
+    
 }//end main function
